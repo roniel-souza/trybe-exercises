f81bba3f5161100ca03b014420b2dcda37950d17
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 19:55:34 -0400

    segundo exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/2.js
@@ -0,0 +1,10 @@
+const a = 8
+const b = 8.1
+if (a>b){
+    console.log("O numero " +a+ " é maior")
+}
+else if (a<b){
+    console.log("O número " +b+ " é maior")
+} else{
+    console.log("Os números " +a+ " e " +b+ " são iguais")
+}
