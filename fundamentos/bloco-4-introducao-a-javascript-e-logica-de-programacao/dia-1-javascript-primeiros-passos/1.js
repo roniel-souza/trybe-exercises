eef4c38b08497087ba5621c6a330a53f4c50eb02
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 19:28:24 -0400

    primeiro exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/1.js
@@ -0,0 +1,12 @@
+const a = 8
+const b = 7
+let soma = a + b
+let subtracao = a - b
+let multiplicacao = a * b
+let divisao = a / b
+let modulo = a % b
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(modulo)
