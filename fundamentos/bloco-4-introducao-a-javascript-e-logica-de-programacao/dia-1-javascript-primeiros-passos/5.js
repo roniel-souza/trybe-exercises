e753d99c0ca3e98dfbeeb11e79f21eed1f1c49c6
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 20:48:15 -0400

    quinto exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/5.js
@@ -0,0 +1,13 @@
+const a = -45
+const b = -45
+const c = 270
+let angulo = a+b+c
+if (angulo < 0){
+    console.log("ERRO ÂNGULO INVÁLIDO!")
+}
+else if (angulo != 180 ){
+    console.log("false")
+}
+else {
+    console.log("true")
+}
