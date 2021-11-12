128c70cadc9d37e279bd7477850bf9ef17bbdda0
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 22:06:48 -0400

    setimo exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/7.js
@@ -0,0 +1,21 @@
+let nota = 76
+if (nota >= 90 && nota < 100){
+    console.log("PORCENTAGEM CONVERTIDA EM: A!!")
+}
+else if (nota >= 80 && nota < 90){
+    console.log("PORCENTAGEM CONVERTIDA EM: B!!")
+} 
+else if (nota >= 70 && nota < 80){
+    console.log("PORCENTAGEM CONVERTIDA EM: C!!")
+}
+else if (nota >= 60 && nota < 70){
+    console.log("PORCENTAGEM CONVERTIDA EM: D!!")
+} 
+else if (nota >= 50 && nota < 60){
+    console.log("PORCENTAGEM CONVERTIDA EM: E!!")
+} 
+else if (nota > 0 && nota < 50){
+    console.log("PORCENTAGEM CONVERTIDA EM: F!!")
+} else {
+    console.log("ERRO!! PROGRAMA ENCERRADO!!")
+}
