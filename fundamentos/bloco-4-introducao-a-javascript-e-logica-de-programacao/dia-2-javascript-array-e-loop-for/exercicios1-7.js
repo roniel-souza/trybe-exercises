8398c3cbabbdb4b285b341b9744f510316f57d01
roniel-souza <ronielbritod@hotmail.com>
2021-11-12 11:25:07 -0400

    exercicios finalizados

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios1-7.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios1-7.js
@@ -1,1 +1,48 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(let number of numbers){
+    // console.log(number);
+}
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index]
+}
+// console.log(soma);
+let mediaAritmetica = soma / (numbers.length - 1);
+// console.log(mediaAritmetica);
+if (mediaAritmetica > 20){
+    // console.log("valor maior que 20");
+} else {
+    // console.log("valor menor ou igual a 20");
+}
+let bigNumber = numbers[0];
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > bigNumber){
+        bigNumber = numbers[index]
+    }
+}
+// console.log(bigNumber)
+let cont = 0
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 != 0) {
+        cont = cont + 1;
+        }
+}
+if (cont > 0){
+    // console.log(cont)
+} else {
+    // console.log("nenhum valor ímpar encontrado")
+}
+let smallNumber = numbers[0]
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < smallNumber){
+        smallNumber = numbers[index]
+    }
+}
+// console.log(smallNumber);
+let array = [];
+for (index = 1; index < 26; index += 1){
+    // array.push(index);
+    array.push(index / 2);
+}
+console.log(array);
+
