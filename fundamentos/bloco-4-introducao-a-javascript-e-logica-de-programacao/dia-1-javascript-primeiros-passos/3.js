bcdf2238fa74458558ca31fab20614abc53c1a52
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 20:07:53 -0400

    exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/3.js
@@ -0,0 +1,12 @@
+const a = 8
+const b = 8.1
+const c = 8.2
+if (a>b && a>c){
+    console.log("O numero " +a+ " é o maior número dos três números!")
+}
+else if (b>a && b>c){
+    console.log("O número " +b+ " é maior número dos três números!")
+}
+else if (c>a && c>b){
+    console.log("O número " +c+ " é maior número dos três números!")
+}
