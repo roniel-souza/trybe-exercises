76d0d8dc6945aeda7625c2975ebc29f9680eae7b
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 22:24:55 -0400

    nono exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/9.js
@@ -0,0 +1,11 @@
+const a = 4
+const b = 4
+const c = 3
+let div1 = a%2
+let div2 = b%2
+let div3 = c%2
+if(div1 > 0 || div2 > 0 || div3 > 0){
+    console.log(true)    
+} else {
+    console.log(false)
+}
