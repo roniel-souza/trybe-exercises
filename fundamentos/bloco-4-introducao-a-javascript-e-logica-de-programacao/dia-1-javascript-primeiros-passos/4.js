51aedd4f640081979dd26b733549a4f5bf582164
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 20:15:34 -0400

    quarto exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.js
@@ -0,0 +1,10 @@
+const a = -1
+if (a<0){
+    console.log("O numero " +a+ " é NEGATIVO!")
+}
+else if (a>0){
+    console.log("O número " +a+ " é POSTIVO!")
+}
+else {
+    console.log("O número " +a+ " é igual a ZERO")
+}
