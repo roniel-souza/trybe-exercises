210a72193551bb3ae4af10652f3e91243e32694b
roniel-souza <ronielbritod@hotmail.com>
2021-11-11 22:38:12 -0400

    decimo exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/10.js
@@ -0,0 +1,5 @@
+const custoProduto = 10
+const valorDeVenda = 16
+let lucroPorProduto = valorDeVenda - custoProduto
+let lucroFinal = 1000 * lucroPorProduto
+console.log("Ao vender 1000 produtos a empresa lucrará: R$ " +lucroFinal+ " reais.")
