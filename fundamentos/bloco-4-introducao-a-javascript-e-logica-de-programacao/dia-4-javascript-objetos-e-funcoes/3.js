db347dc40b757478e3694f02eaac5b2976811bbd
roniel-souza <ronielbritod@hotmail.com>
2021-11-13 16:04:39 -0400

    resposta 3 - easy

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/3.js
@@ -0,0 +1,9 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    note: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+info['recorrente'] = 'sim';
+for (let index in info){
+    console.log(index);
+}
