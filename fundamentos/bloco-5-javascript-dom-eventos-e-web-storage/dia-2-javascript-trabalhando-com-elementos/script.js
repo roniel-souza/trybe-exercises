0486dc68da402e5fb07420e468aa85cbf8558c42
roniel-souza <ronielbritod@hotmail.com>
2021-11-17 17:43:18 -0400

    answer 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,14 +1,12 @@
 // Exercício 1
-
 let h1 = document.createElement('h1');
 h1.innerText = 'Exercício 5.2 - JavaScript';
 document.body.appendChild(h1);
-
 // Exercício 2
-
 let main = document.createElement('main');
 main.className = 'main-content';
 document.body.appendChild(main);
-console.log(document.body);
-
-
+//Exercício 3
+let section = document.createElement('section');
+section.className = 'center-content';
+main.appendChild(section);
