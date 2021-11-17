c11d2e80c25828a54dfb8bf037528a32035217b4
roniel-souza <ronielbritod@hotmail.com>
2021-11-16 21:53:36 -0400

    fim dos exercícios

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -6,4 +6,10 @@ document.getElementsByTagName('p')[0].innerText = 'Ganhar 10k por mês!'
 document.getElementsByTagName('p')[0].style="text-align:center"
 // (rgb(76,164,109)
 document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
-
+document.getElementsByTagName('section')[0].style.backgroundColor = 'white';
+document.getElementsByClassName('title')[0].innerText = 'Exercício 5.1 - JavaScript!';
+document.getElementsByTagName('p')[0].innerText = document.getElementsByTagName('p')[0].innerText.toUpperCase();
+document.getElementsByTagName('p');
+for (let index = 0; index < document.getElementsByTagName('p').length; index += 1) {
+    console.log(document.getElementsByTagName('p')[index].innerText) 
+}
