aa9bf0255c0399b1fa3060b98d726bc5921e9554
roniel-souza <ronielbritod@hotmail.com>
2021-11-16 22:29:11 -0400

    correções finais

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,15 +1,35 @@
 document.getElementsByTagName('p');
 for (let index = 0; index < document.getElementsByTagName('p').length; index += 1) {
+    console.log(document.getElementsByTagName('p')[index].innerText) 
+}
+
+function mudaTexto (string){
+    for (let index = 0; index < document.getElementsByTagName('p').length; index += 1) {
     document.getElementsByTagName('p')[index].innerText = '' 
 }
-document.getElementsByTagName('p')[0].innerText = 'Ganhar 10k por mês!'
-document.getElementsByTagName('p')[0].style="text-align:center"
-// (rgb(76,164,109)
-document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
-document.getElementsByTagName('section')[0].style.backgroundColor = 'white';
-document.getElementsByClassName('title')[0].innerText = 'Exercício 5.1 - JavaScript!';
-document.getElementsByTagName('p')[0].innerText = document.getElementsByTagName('p')[0].innerText.toUpperCase();
-document.getElementsByTagName('p');
-for (let index = 0; index < document.getElementsByTagName('p').length; index += 1) {
+document.getElementsByTagName('p')[0].innerText = string;
+document.getElementsByTagName('p')[0].style="text-align:center";
+}
+
+function mudaCor (cor){
+    document.getElementsByClassName('main-content')[0].style.backgroundColor = cor;
+}
+
+function mudaCorMeio (cor){
+    document.getElementsByTagName('section')[0].style.backgroundColor = cor;
+}
+
+function corrijaOTexto (textoCorreto){
+    document.getElementsByClassName('title')[0].innerText = textoCorreto;
+}
+
+function textoMaiusculo (){
+    document.getElementsByTagName('p')[0].innerText = document.getElementsByTagName('p')[0].innerText.toUpperCase();
+}
+
+function exibirTodosParagrafos (){
+    document.getElementsByTagName('p');
+    for (let index = 0; index < document.getElementsByTagName('p').length; index += 1) {
     console.log(document.getElementsByTagName('p')[index].innerText) 
+}
 }
